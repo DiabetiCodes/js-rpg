f252af5e576012958b14d8918a10eb23cb3a81d3
Tonyobyo <michael.mastrucci@gmail.com>
2020-04-29 11:31:15 -0400

    encapsulation
    Moved characterController code to separate file

---
+++ js/characterController.js
@@ -0,0 +1,74 @@
+var keysDown = {
+	37 : false,
+	38 : false,
+	39 : false,
+	40 : false
+};
+
+var player = new Character();
+
+function Character()
+{
+	this.tileFrom	= [1,1];
+	this.tileTo		= [1,1];
+	this.timeMoved	= 0;
+	this.dimensions	= [30,30];
+	this.position	= [45,45];
+	this.delayMove	= 700;
+}
+Character.prototype.placeAt = function(x, y)
+{
+	this.tileFrom	= [x,y];
+	this.tileTo		= [x,y];
+	this.position	= [((tileW*x)+((tileW-this.dimensions[0])/2)),
+		((tileH*y)+((tileH-this.dimensions[1])/2))];
+};
+Character.prototype.processMovement = function(t)
+{
+	if(this.tileFrom[0]==this.tileTo[0] && this.tileFrom[1]==this.tileTo[1]) { return false; }
+
+	if((t-this.timeMoved)>=this.delayMove)
+	{
+		this.placeAt(this.tileTo[0], this.tileTo[1]);
+	}
+	else
+	{
+		this.position[0] = (this.tileFrom[0] * tileW) + ((tileW-this.dimensions[0])/2);
+		this.position[1] = (this.tileFrom[1] * tileH) + ((tileH-this.dimensions[1])/2);
+
+		if(this.tileTo[0] != this.tileFrom[0])
+		{
+			var diff = (tileW / this.delayMove) * (t-this.timeMoved);
+			this.position[0]+= (this.tileTo[0]<this.tileFrom[0] ? 0 - diff : diff);
+		}
+		if(this.tileTo[1] != this.tileFrom[1])
+		{
+			var diff = (tileH / this.delayMove) * (t-this.timeMoved);
+			this.position[1]+= (this.tileTo[1]<this.tileFrom[1] ? 0 - diff : diff);
+		}
+
+		this.position[0] = Math.round(this.position[0]);
+		this.position[1] = Math.round(this.position[1]);
+	}
+
+	return true;
+}
+
+function toIndex(x, y)
+{
+	return((y * mapW) + x);
+}
+
+window.onload = function()
+{
+	ctx = document.getElementById('game').getContext("2d");
+	requestAnimationFrame(drawGame);
+	ctx.font = "bold 10pt sans-serif";
+
+	window.addEventListener("keydown", function(e) {
+		if(e.keyCode>=37 && e.keyCode<=40) { keysDown[e.keyCode] = true; }
+	});
+	window.addEventListener("keyup", function(e) {
+		if(e.keyCode>=37 && e.keyCode<=40) { keysDown[e.keyCode] = false; }
+	});
+};
